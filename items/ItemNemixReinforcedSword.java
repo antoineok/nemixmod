59005253158f61b985ababd05777907d2e27453e
Nem <54665744+Nem-developing@users.noreply.github.com>
2020-04-02 18:26:49 +0200

    Add files via upload

---
+++ items/ItemNemixReinforcedSword.java
@@ -0,0 +1,12 @@
+package com.mod.nemixmod.items;
+
+import net.minecraft.item.ItemSword;
+
+public class ItemNemixReinforcedSword extends ItemSword 
+{
+
+	public ItemNemixReinforcedSword(ToolMaterial p_i45356_1_) {
+		super(p_i45356_1_);
+	}
+
+}
