291b60a41f80cf73b8bd75d63ac62af761861c13
Nem <54665744+Nem-developing@users.noreply.github.com>
2020-04-02 18:23:07 +0200

    Add files via upload

---
+++ Reference.java
@@ -0,0 +1,10 @@
+package com.mod.nemixmod;
+
+public class Reference {
+
+	public static final String MOD_ID = "nemixmod";
+	public static final String MOD_NAME = "Nemix Mod";
+	public static final String MOD_VERSION = "1.0.4";
+	public static final String CLIENT_PROXY = "com.mod.nemixmod.proxy.ClientProxy";
+	public static final String SERVER_PROXY = "com.mod.nemixmod.proxy.CommonProxy";
+}
